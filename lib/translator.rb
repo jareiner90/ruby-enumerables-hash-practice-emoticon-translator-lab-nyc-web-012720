87bc02bc1b1e7ab962ffe900501b71450ac03dde
John Reiner <johnreiner1990@gmail.com>
2020-01-15 22:36:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 def get_japanese_emoticon(file, emoticon)
   lib = load_library(file)
   if lib["get_emoticon"][emoticon] == nil
-    puts "sorry not found"
+    puts "Sorry, that emoticon was not found"
   else
     lib["get_emoticon"][emoticon]
   end
