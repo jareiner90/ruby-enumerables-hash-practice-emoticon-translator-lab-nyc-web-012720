7ee20d6e5ac1b0fc3c556ff62a4f3ca2a134ab8a
John Reiner <johnreiner1990@gmail.com>
2020-01-15 22:17:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,9 @@ def load_library(library)
     "get_meaning" => {},
     "get_emoticon" => {}
   }
-
+  library.each do |key, value|
+    
+  end
 end
 
 # def get_japanese_emoticon(file, emoticon)
